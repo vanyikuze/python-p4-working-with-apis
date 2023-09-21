378b89803f20e8cc321e9d83eaa54b6f967831c4
Vany <vany.ikuze@student.moringaschool.com>
2023-09-21 09:37:30 +0300

    Done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -3,6 +3,15 @@ import requests
 import json
 
 class GetPrograms:
+  
+  def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+            programs_list.append(program["agency"])
+
+    return programs_list
 
   def get_programs(self):
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
@@ -11,5 +20,13 @@ class GetPrograms:
     return response.content
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+# programs = GetPrograms().get_programs()
+# print(programs)
+
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
+    
+    
